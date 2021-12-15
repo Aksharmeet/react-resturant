44dd8ea13b86a63c2191ac8d8996d5d81f602bcd
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2021-12-15 08:58:28 +0530

    menu styling updates

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -7,6 +7,7 @@ import { useState } from 'react';
 function Menu() {
     let Veg = IndianMenu.filter((items) => (items.category == "vegMain"));
     let NonVeg = IndianMenu.filter((items) => (items.category == "nonMain"));
+    let Biryani = IndianMenu.filter((items) => (items.category == "biryani"));
     let Breads = IndianMenu.filter((items) => (items.category == "breads"));
     let Deserts = IndianMenu.filter((items) => (items.category == "deserts"));
     let Beverages = IndianMenu.filter((items) => (items.category == "beverages"));
@@ -31,13 +32,13 @@ function Menu() {
         <MenuBody>
           
           <Categories SideMenu={SideMenu}>
-                <ul>
-                <li>Curries (VEG)</li>
-                <li>Curries(NON-VEG)</li>
-                <li>Briyani</li>
-                <li>Breads</li>
-                <li>Deserts</li>
-                <li>Beverages</li>
+                <ul onClick ={OpenClose}>
+                <a href="#veg"><li>Curries (VEG)</li></a>
+                <a href="#nonVeg"><li>Curries(NON-VEG)</li></a>
+                <a href="#biryani"><li>Briyani</li></a>
+                <a href="#breads"><li>Breads</li></a>
+                <a href="#deserts"><li>Deserts</li></a>
+                <a href="#beverages"><li>Beverages</li></a>
                 </ul>
                 <SideMenuWrapper onClick={OpenClose} onMouseOver={() => setMouseMenu(true)} onMouseOut={() =>setMouseMenu(false)}>
                     <FontAwesomeIcon icon={faUtensils} style = {MouseMenu ? {color: "#fff"}:{color:"#000"}} size="lg"/>
@@ -49,30 +50,105 @@ function Menu() {
           <FoodWrapper>
         
 
-          <VegContainer>
-           <h4>Veg Items</h4><div></div>
-          {Veg.map((item) => <img key={item.img} src={item.img}/>)}
-          </VegContainer>
+          
+            <div id="veg">
+            <h4>Veg Items</h4>
+            {Veg.map((item) => 
+                (
+                    <VegContainer>
+                    
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </VegContainer>
+                )
+            )}
+            </div>
+          
+         
+            <div id="nonVeg">
+                 <h4>Non-Veg Items</h4>
+         
+                {NonVeg.map((item) => 
+                (  
+                    <NonVegContainer>
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </NonVegContainer>
+                    
+                )
+                )}
+            </div>
+         
+            <div id="biryani">
+                 <h4>Biryani</h4>
+         
+                {Biryani.map((item) => 
+                (  
+                    <BiryaniContainer>
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </BiryaniContainer>
+                    
+                )
+                )}
+            </div>
 
-          <NonVegContainer>
-          <h4>Non-Veg Items</h4><div></div>
-          {NonVeg.map((item) => <img key={item.img} src={item.img}/>)}
-          </NonVegContainer>
+            <div id="breads">
+                 <h4>Breads</h4>
+         
+                {Breads.map((item) => 
+                (  
+                    <BreadsContainer>
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </BreadsContainer>
+                    
+                )
+                )}
+            </div>
+        
+            <div id="deserts">
+                 <h4>Deserts</h4>
+         
+                {Deserts.map((item) => 
+                (  
+                    <DesertsContainer>
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </DesertsContainer>
+                    
+                )
+                )}
+            </div>
+        
 
-          <BriyaniContainer>
-          <h4>Briyani</h4><div></div>
-          {Breads.map((item) => <img key={item.img} src={item.img}/>)}
-          </BriyaniContainer>
-
-          <DesertsContainer>
-          <h4>Deserts</h4><div></div>
-          {Deserts.map((item) => <img key={item.img} src={item.img}/>)}
-          </DesertsContainer>
-
-          <BeveragesContainer>
-          <h4>Beverages</h4><div></div>
-          {Beverages.map((item) => <img key={item.img} src={item.img}/>)}
-          </BeveragesContainer>
+          
+            <div id="beverages">
+                 <h4>Beverages</h4>
+         
+                {Beverages.map((item) => 
+                (  
+                    <BeveragesContainer>
+                    <img key={item.img} src={item.img}/>
+                    <h5>{item.title}</h5>
+                    <p>{item.disc}</p>
+                    <p> &#8377; {item.price}</p>
+                    </BeveragesContainer>
+                    
+                )
+                )}
+            </div>
+         
           </FoodWrapper>
             
         
@@ -84,10 +160,12 @@ const MenuBody = styled.div`
     background-color:#100000;
     color:#fff;
     display:flex;
-    padding-top:150px;
+    padding-top:100px;
    
         h4{
-            margin:30px;
+            padding-top:30px;
+            margin:0px auto 30px auto;
+            text-align:center;
             font-family:cinzel;
             font-size:2rem;
             font-weight:200;
@@ -99,6 +177,10 @@ const MenuBody = styled.div`
             box-shadow: 8px 8px #feaa1a;
             margin:40px 30px;
             
+        };
+        a{
+            color:#000;
+            text-decoration:none;
         }
        
 
@@ -151,6 +233,11 @@ const Categories = styled.div`
         font-size:1rem;
 
     }
+  
+    li{
+        cursor:pointer;
+       
+    }
     @media (max-width:720px){
         ul{
             position:fixed;
@@ -163,6 +250,7 @@ const Categories = styled.div`
             transform:${props => props.SideMenu ? "translateY(70vw) translateX(-50vw) Scale(0, 0)" : "translateY(0) translateX(0) Scale(1, 1)"};
         }
         li{
+         
             border-bottom: 1px solid #0000003e;
             padding:10px 20px;
             transition:all .2s;
@@ -183,6 +271,9 @@ const Categories = styled.div`
     }
 `
 const FoodWrapper = styled.div`
+    height:100vh ;
+    overflow-y: scroll;
+    scroll-behavior: smooth;
 `
 const VegContainer = styled.div`
     div{
@@ -202,13 +293,31 @@ const VegContainer = styled.div`
             width:60%;
             max-width:none;
         }
-    
-  
-    
-
 `
 
 const NonVegContainer = styled.div`
+    
+    div{
+        background-color:#ffffff80;
+        height:1px;
+        margin:auto 30px;
+        
+    
+    
+    }
+    @media (max-width:720px){
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        flex-wrap:wrap;
+        img{
+            width:60%;
+            max-width:none;
+        }
+`
+
+const BreadsContainer = styled.div`
     div{
         background-color:#ffffff80;
         height:1px;
@@ -227,14 +336,13 @@ const NonVegContainer = styled.div`
             max-width:none;
         }
 `
-
-const BriyaniContainer = styled.div`
+const BiryaniContainer = styled.div`
     div{
         background-color:#ffffff80;
         height:1px;
         margin:auto 30px;
-    
-    
+
+
     }
     @media (max-width:720px){
         display:flex;
@@ -245,9 +353,8 @@ const BriyaniContainer = styled.div`
         img{
             width:60%;
             max-width:none;
-        }
+        }   
 `
-
 const DesertsContainer = styled.div` 
     div{
         background-color:#ffffff80;
